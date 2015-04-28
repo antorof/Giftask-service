7b45644c2bdb70f5f38341ebaba8021fbaad56f6
antorof <antorof.dev@gmail.com>
2015-04-28 17:53:38 +0200

    Ajuste puertos y URLs

--- app.js
+++ app.js
@@ -11,7 +11,17 @@ var bodyParser = require('body-parser');
  */
 var mongo = require('mongodb');
 var monk = require('monk');
-var db = monk('192.168.58.133:27017/nodetest1');
+
+var mongodb_url = '127.0.0.1:27017/giftask';
+if(process.env.OPENSHIFT_MONGODB_DB_PASSWORD){
+  mongodb_url = process.env.OPENSHIFT_MONGODB_DB_USERNAME + ":" +
+  process.env.OPENSHIFT_MONGODB_DB_PASSWORD + "@" +
+  process.env.OPENSHIFT_MONGODB_DB_HOST + ':' +
+  process.env.OPENSHIFT_MONGODB_DB_PORT + '/' +
+  process.env.OPENSHIFT_APP_NAME;
+}
+
+var db = monk(mongodb_url);
 
 
 /**
