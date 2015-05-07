16b03de62b175c73655a36ce6fa18ee098648306
antorof <antorof.dev@gmail.com>
2015-05-07 16:31:19 +0200

    Parser de ficheros y expongo IP para Debug.

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+var multer  = require('multer')
 
 
 /**
@@ -45,6 +46,7 @@ app.set('view engine', 'jade');
 // uncomment after placing your favicon in /public
 //app.use(favicon(__dirname + '/public/favicon.ico'));
 app.use(logger('dev'));
+app.use(multer({ dest: './uploads/tmp/'}))
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
